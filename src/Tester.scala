d1032df3224835a8e2dad61d9cce432b3fe93ccc
Dulshan <dulshanr12@gmail.com>
2019-08-18 19:23:27 +0530

    added all

---
+++ src/Tester.scala
@@ -0,0 +1,16 @@
+object Tester {
+  def main(args: Array[String]): Unit = {
+    println("hello World")
+    val answer:String ="Meeeow"
+
+    println(answer.charAt(0))
+    println(answer.charAt(answer.length-1))
+
+    var properString: String="Random_String"
+
+    var editedString:String =properString.dropRight(3)
+    println(editedString)
+
+    //added the lines to see whether they get fetched
+  }
+}
